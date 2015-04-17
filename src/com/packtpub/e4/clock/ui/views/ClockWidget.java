417e9768d3646e8333d92e8b715a850023cac467
SamBarn <sbionicle@gmail.com>
2015-04-17 13:49:12 -0600

    Update ClockWidget.java

--- src/com/packtpub/e4/clock/ui/views/ClockWidget.java
+++ src/com/packtpub/e4/clock/ui/views/ClockWidget.java
@@ -4,13 +4,17 @@ import java.util.Date;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.*;
 
 public class ClockWidget extends Canvas{
-	
-	public ClockWidget(Composite parent, int style) {
+	private final Color color;
+	public ClockWidget(Composite parent, int style, RGB rgb) {
 		super(parent, style);
+		//FIXME color is leaked!
+		this.color = new Color(parent.getDisplay(),rgb);
 		
 		addPaintListener(new PaintListener() {
 			public void paintControl(PaintEvent e) {
@@ -39,12 +43,12 @@ public class ClockWidget extends Canvas{
 	}
 	
 	@SuppressWarnings("deprecation")
-	public void paintControl(PaintEvent e) {
+	protected void paintControl(PaintEvent e) {
 		e.gc.drawArc(e.x,e.y,e.width-1,e.height-1,0,360);
 		int seconds = new Date().getSeconds();
 		int arc = (15-seconds) * 6 % 360;
-		org.eclipse.swt.graphics.Color blue = e.display.getSystemColor(SWT.COLOR_BLUE);
-		e.gc.setBackground(blue);
+		//org.eclipse.swt.graphics.Color blue = e.display.getSystemColor(SWT.COLOR_BLUE);
+		e.gc.setBackground(color);
 		e.gc.fillArc(e.x,e.y,e.width-1,e.height-1,arc-1,2);
 	}
 	
@@ -61,5 +65,5 @@ public class ClockWidget extends Canvas{
 			size = 50;
 		return new Point(size,size);
 	}
-	 
+	
 }
